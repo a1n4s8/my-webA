c181f65b1eb6d4a64f567c20d3c6fbb0f4daa0c3
DESKTOP-Q9VG6OD\user <asdf@naver.com>
2018-10-24 17:43:41 +0900

    init-commit

---
+++ src/main/java/io/wma/MyWebAApplication.java
@@ -0,0 +1,12 @@
+package io.wma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAApplication.class, args);
+	}
+}
